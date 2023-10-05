a6c576aa27acc6cb952ded5006fe38b91383b954
Gustavo Briceño <gustav.dev.code@gmail.com>
2023-10-04 22:22:51 -0500

    Responsive de la página

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".nav");
+const hamburguer = document.querySelector(".hamburguer");
+const menuItems = document.querySelectorAll(".nav ul li a");
+
+hamburguer.addEventListener("click", () => {
+  nav.classList.toggle("nav--open");
+  hamburguer.classList.toggle("x");
+});
+
+function closeMenu() {
+  nav.classList.remove("nav--open");
+  hamburguer.classList.remove("x");
+}
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", closeMenu);
+});
